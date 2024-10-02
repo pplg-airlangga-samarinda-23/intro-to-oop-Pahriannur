064449ed4b908ae70323d21183b50eee8ed890f2
pahrian_Nur <pahriannur217@gmail.com>
2024-10-02 21:58:29 +0800

    tugas

--- soal-1/Kotak.php
+++ soal-1/Kotak.php
@@ -2,25 +2,32 @@
 class PersegiPanjang
 {
     // deklarasikan panjang dan lebar
+    public $panjang;
+    public $lebar;
 
 
     // deklarasikan metode konstruktor
-
+    public function _construct($panjang, $lebar){
+        $this->panjang =$panjang;
+        $this->lebar =$lebar;
+    }
 
     // deklarasikan metode mutator dan asesor
-
+        public function _get($atribut){
+            return $this->$atribut;
+        }
 
 
     public function hitungLuas()
     {
         // lengkapi kode untuk menghitung luas persegi panjang
-
+        return $this->panjang * $this->lebar;
     }
 
     public function hitungKeliling()
     {
         // lengkapi dengan kode untuk menghitung keliling persegi panjang
-
+        return 2 * ($this->panjang + $this->lebar);
     }
 }
 
